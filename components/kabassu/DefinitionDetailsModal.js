23182d1e79bfd13820b7cfc45fc7b49a2b8dccbe
Daniel Madejek <daniel.madejek@cognifide.com>
2019-08-22 11:51:29 +0200

    finished definition modal

--- components/kabassu/DefinitionDetailsModal.js
+++ components/kabassu/DefinitionDetailsModal.js
@@ -7,9 +7,9 @@ class DefinitionDetailsModal extends React.Component {
     super(props);
   }
 
-
   fetchData() {
-    fetch(process.env.kabassuServer + '/kabassu/getdefinition/'+this.props.definitionId, {
+    fetch(process.env.kabassuServer + '/kabassu/getdefinition/'
+        + this.props.definitionId, {
       crossDomain: true,
       method: 'GET',
     })
@@ -30,27 +30,53 @@ class DefinitionDetailsModal extends React.Component {
     )
   }
 
-  componentDidUpdate(prevProps){
-    if (this.props.definitionId!== null && this.props.definitionId !== prevProps.definitionId) {
+  componentDidUpdate(prevProps) {
+    if (this.props.definitionId !== null && this.props.definitionId
+        !== prevProps.definitionId) {
       this.fetchData()
     }
   }
 
   render() {
     let modal;
-    if (this.props.definitionId !== null && this.state!==null && this.state.isLoaded) {
-      console.log(this.state)
+    if (this.props.definitionId !== null && this.state !== null
+        && this.state.isLoaded) {
       modal = <Modal show={this.props.show}
-                         onHide={this.props.onHide}
-                         size="lg"
-                         aria-labelledby="contained-modal-title-vcenter"
-                         centered>
+                     onHide={this.props.onHide}
+                     size="lg"
+                     aria-labelledby="contained-modal-title-vcenter"
+                     centered>
         <Modal.Header closeButton>
-          <Modal.Title>Definition
-            details {this.state.result.name} </Modal.Title>
+          <Modal.Title>
+            <div className="alert alert-dark" role="alert">
+              Definition {this.state.result.name}
+            </div>
+          </Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <div>{this.state.result.name}</div>
+
+          <table className="table table-hover table-bordered">
+            <tbody>
+            <tr>
+              <td>Runner</td>
+              <td>
+                {this.state.result.runner}
+              </td>
+            </tr>
+            <tr>
+              <td>Location Type</td>
+              <td>
+                {this.state.result.locationType}
+              </td>
+            </tr>
+            <tr>
+              <td>Location</td>
+              <td>
+                {this.state.result.location}
+              </td>
+            </tr>
+            </tbody>
+          </table>
         </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={this.props.onHide}>
